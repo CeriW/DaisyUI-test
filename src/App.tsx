8fe99215238d5ab2cb94b11080d06d1a5f1439db
Ceri Woolway <ceriwoolway@gmail.com>
2026-01-12 13:34:10 +0000

    Some basic buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.scss'
 
 function App() {
@@ -8,7 +6,7 @@ function App() {
 
   return (
     <>
-      <div>
+      {/* <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -27,7 +25,10 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
+      <button className="btn btn-primary">Primary button</button>
+      <button className="btn btn-secondary">Secondary button</button>
+
     </>
   )
 }
