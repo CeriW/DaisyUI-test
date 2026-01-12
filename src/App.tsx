fed4b68690172811580e0aee2de2d04352ad8414
Ceri Woolway <ceriwoolway@gmail.com>
2026-01-12 13:45:43 +0000

    Styled buttons

--- src/App.tsx
+++ src/App.tsx
@@ -26,8 +26,27 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p> */}
-      <button className="btn btn-primary">Primary button</button>
-      <button className="btn btn-secondary">Secondary button</button>
+      <div style={{ marginBottom: '20px' }}>
+        <button className="btn btn-primary ceri-button ceri-button-primary">Primary button</button>
+        <button className="btn btn-secondary ceri-button ceri-button-secondary">Secondary button</button>
+      </div>
+
+      <div className="card">
+        <figure>
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+            alt="Shoes" />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">Card Title</h2>
+          <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+          <div className="card-actions">
+            <button className="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+      </div>
+
+
 
     </>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,4 +3,17 @@ import daisyui from "daisyui";
 /** @type {import('tailwindcss').Config} */
 export default {
   plugins: [daisyui],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#282439",      // Your custom primary color
+          "secondary": "#534f66",    // Your custom secondary color
+          "accent": "#37cdbe",
+          "neutral": "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+    ],
+  },
 };
